b3b0b1e3e09a6007758204538252cefe89f84f5b
Hektor Zaimi <hektorzaimi.dev@gmail.com>
2023-10-16 18:22:51 +0200

    Format Files

--- src/App.js
+++ src/App.js
@@ -3,14 +3,14 @@ import Auth from "./components/auth";
 import Cookies from "universal-cookie";
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
-import {Chat} from "./components/chat"
+import { Chat } from "./components/chat";
 
 const cookies = new Cookies();
 
 function App() {
   const [loggedIn, setLoggedIn] = useState(cookies.get("auth-token"));
   const [room, setRoom] = useState("");
-  const roomInput = useRef(null)
+  const roomInput = useRef(null);
   if (!loggedIn) {
     return (
       <div className="App">
@@ -20,19 +20,38 @@ function App() {
   }
   return (
     <>
-      <div>{room ? <div> Chat <Chat room={room}></Chat></div> : <div className="room">
-       
-      <Form className="center">
-      <Form.Group className="mb-3" controlId="formBasicEmail">
-        <Form.Label>Enter the room name</Form.Label>
-        <Form.Control ref={roomInput} type="text" placeholder="Room name" />
-        <Button onClick={() => {
-          setRoom(roomInput.current.value)
-        }} style={{marginTop: 8}}>Join Room</Button>
-        <p className="text-muted"  >Note: please press the button, pressing ENTER does not work.</p>
-      </Form.Group>
-      </Form>
-        </div>}</div>
+      <div>
+        {room ? (
+          <div>
+            {" "}
+            Chat <Chat room={room}></Chat>
+          </div>
+        ) : (
+          <div className="room">
+            <Form className="center">
+              <Form.Group className="mb-3" controlId="formBasicEmail">
+                <Form.Label>Enter the room name</Form.Label>
+                <Form.Control
+                  ref={roomInput}
+                  type="text"
+                  placeholder="Room name"
+                />
+                <Button
+                  onClick={() => {
+                    setRoom(roomInput.current.value);
+                  }}
+                  style={{ marginTop: 8 }}
+                >
+                  Join Room
+                </Button>
+                <p className="text-muted">
+                  Note: please press the button, pressing ENTER does not work.
+                </p>
+              </Form.Group>
+            </Form>
+          </div>
+        )}
+      </div>
     </>
   );
 }
--- src/firebase-config.js
+++ src/firebase-config.js
@@ -1,7 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import {getAuth, GoogleAuthProvider} from "firebase/auth"
-import {getFirestore} from "firebase/firestore"
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -12,7 +12,7 @@ const firebaseConfig = {
   projectId: "chatapp-room",
   storageBucket: "chatapp-room.appspot.com",
   messagingSenderId: "358312345021",
-  appId: "1:358312345021:web:8a40b07e4979ff46bee6d4"
+  appId: "1:358312345021:web:8a40b07e4979ff46bee6d4",
 };
 
 // Initialize Firebase
